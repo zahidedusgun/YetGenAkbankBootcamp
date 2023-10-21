feb1da97f56012f8cc7d6fc5eeeab642d3a85560
zahidsgn <zahide.dusgun@gmail.com>
2023-10-21 18:54:35 +0300

    MVC part has been updated.

---
+++ ProductifyApp/Productify/Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult GetAll()
+        {
+            return View();
+        }
+
+        // GET: AddProductController
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
