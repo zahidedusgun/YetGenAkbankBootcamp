e4b57a94e8b8c848513267e63e1fe693c2a8cb75
zahidsgn <zahide.dusgun@gmail.com>
2023-10-21 18:47:48 +0300

    ShopifyApp updated

---
+++ ShopifyApp/Context/ShopifyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopifyApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopifyApp.Context
+{
+    public class ShopifyDbContext : DbContext
+    {
+        public DbSet<Product> Products{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+            optionsBuilder.UseNpgsql("Server=;Port=;Database=;User Id=;Password=;\r\n");
+    }
+    }
+}
---
+++ ShopifyApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using ShopifyApp.Context;
+using ShopifyApp.Entities;
+
+Console.WriteLine("PostgreSQL Exercise");
+
+ShopifyDbContext _context = new();
+
+#region AddSingle
+Product product1 = new Product("Laptop", 2.5m, 999.99m);
+_context.Products.Add(product1);
+
+_context.SaveChanges();
+#endregion
+
+#region AddMultiple
+List<Product> Allproducts = new List<Product> 
+{
+     new Product("Smartphone", 0.3m, 699.99m),
+     new Product("Headphones", 0.1m, 49.99m),
+     new Product("Coffee Maker", 4.0m, 149.99m),
+     new Product("Television", 12.0m, 799.99m),
+     new Product("Tablet", 0.5m, 299.99m),
+     new Product("Washing Machine", 25.0m, 599.99m)
+
+};
+_context.Products.AddRange(Allproducts);
+_context.SaveChanges();
+#endregion
