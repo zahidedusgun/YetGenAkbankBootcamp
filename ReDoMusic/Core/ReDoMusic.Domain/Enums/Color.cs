8d509eeab539e2d928b7437e63ac1b1d91fde5b4
zahidsgn <zahide.dusgun@gmail.com>
2023-10-22 23:34:00 +0300

    Inf ad Core part completed of ReDo Music.

---
+++ ReDoMusic/Core/ReDoMusic.Domain/Enums/Color.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Enums
+{
+    public enum Color
+    {
+        Red = 1,
+        Green = 2,
+        Blue = 3,
+        Yellow = 4,
+        Orange = 5,
+        Purple = 6,
+        Pink = 7,
+        Brown = 8,
+        Gray = 9,
+        Cyan = 10
+    }
+
+}
